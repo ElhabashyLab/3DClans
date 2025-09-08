85ac0e828a41c6d9b680cb33f4807825918eafbc
AronWichtner <aronwichtner@gmail.com>
2025-09-08 11:55:07 +0200

    improvements of pipeline

--- Dataset_Generator/main.py
+++ Dataset_Generator/main.py
@@ -7,6 +7,10 @@ class DatasetGenerator:
     Each cluster is defined by a seed sequence and contains very similar sequences.
     The remaining sequences of a cluster are obtained by blasting the seed sequence.
     """    
+    def __init__(self):
+        self.Entrez_email = "aron.wichtner@tuebingen.mpg.de"
+        self.psiblast_results_path = "Dataset_Generator/psiblast_results.xml"
+    
     
     def generate(self, n, n_cl, seeds):
         """
@@ -17,26 +21,35 @@ class DatasetGenerator:
         :return: path to the generated fasta file
         """
         self._check_for_correct_input(n, n_cl, seeds)
-        fasta_content = ""
+        fasta_records = []
         for i in range(n_cl):
-            records_of_cluster_i = []
             seed_uid = seeds[i]
-            # download seed sequence
-            seed_record = self._download_sequences([seed_uid])
-            records_of_cluster_i.append(seed_record)
+            seed_record = self._download_seed_records(seed_uid, i)
+            fasta_records.append(seed_record)
             # blast for n/n_cl - 1 similar sequences
             print(f"Finding similar sequences for cluster {i + 1}...")
-            records_of_similar_sequences = self.get_records_of_similar_sequences(seed_record, n // n_cl - 1)
-            records_of_cluster_i.extend(records_of_similar_sequences)
-            # add sequences of cluster i to fasta_content and mark the cluster
-            for record in records_of_cluster_i:
-                fasta_content += f">Cluster_{i+1}_Sequence_{record.id}\n{str(record.seq)}\n"
+            records_of_similar_sequences = self.get_records_of_similar_sequences(seed_record, n // n_cl - 1, i)
+            fasta_records.extend(records_of_similar_sequences)
         # save the generated fasta content to a file
         output_path = f"example_files/generated_fasta/{n}_seq_{n_cl}_clusters.fasta"
         print(f"Saving fasta file at {output_path}")
-        self._save_to_file(output_path, fasta_content)
+        SeqIO.write(fasta_records, output_path, "fasta")
         return output_path
     
+    
+    def _download_seed_records(self, seed_uid, i):
+        """
+        Downloads the seed sequences from NCBI.
+        :param seed_uid: UID of the seed sequence
+        :param i: index of the cluster of the seed
+        :return: list of SeqRecord objects
+        """
+        sequences = self._download_sequences([seed_uid])
+        seed_record  = sequences[0]
+        seed_record.id = seed_uid
+        seed_record.description = f"Cluster_{i+1}_Seed"
+        return seed_record
+        
     
     def _check_for_correct_input(self, n, n_cl, seeds):
         """
@@ -59,7 +72,7 @@ class DatasetGenerator:
         :param uid: the UID of the sequence
         :return: the sequence as a string
         """
-        Entrez.email = "aron.wichtner@tuebingen.mpg.de"
+        Entrez.email = self.Entrez_email
         uids_as_string = ",".join(uids)
         handle = Entrez.efetch(db="protein", id=uids_as_string, rettype="fasta", retmode="text")
         records = list(SeqIO.parse(handle, "fasta"))
@@ -85,18 +98,20 @@ class DatasetGenerator:
         return result_handle
     
     
-    def get_records_of_similar_sequences(self, record, num_sequences):
+    def get_records_of_similar_sequences(self, record, num_sequences, i):
         """
         Retrieves records of sequences similar to the given record sequence.
         :param record: the record to compare
         :param num_sequences: number of similar records to retrieve
+        :param i: index of the cluster
         :return: list of SeqRecord objects
         """
         results_handle = self._blast_sequence(record, num_sequences)
-        self.psiblast_results_path = "Dataset_Generator/psiblast_results.xml"
         self._save_to_file(self.psiblast_results_path, results_handle.read())
         results_handle.close()
         similar_records = self._parse_hits()
+        for record in similar_records:
+            record.description = f"Cluster_{i+1}_Similar"
         return similar_records
         
         
@@ -132,5 +147,6 @@ class DatasetGenerator:
 seeds = ["P68871"]
 generator = DatasetGenerator()
 generator.generate(9, 1, seeds)
-        
-    
+# something is wrong with the seq.headers
+# something is off with the number of sequences returned by blast...
+# check overwritting of xml file
